eac2259abf8fe25268f74dacc94f6586d25918d1
Jose B Gomes <jbonigomes@yahoo.com.br>
2026-01-30 23:20:42 +0000

    JS cleanup

--- src/index.js
+++ src/index.js
@@ -2,26 +2,22 @@ const KEY = 'news-minimalist'
 
 const getText = (item, tag) => item.getElementsByTagName(tag)[0].textContent
 
-const onClick = ({ target }) => {
-  const allItems = JSON.parse(localStorage.getItem(KEY))
+const onToggle = ({ currentTarget }) => {
+  const { id, open } = currentTarget
 
-  localStorage.setItem(
-    KEY,
-    JSON.stringify({
-      ...allItems,
-      [target.id]: {
-        ...allItems[target.id],
-        read: true,
-      }
-    })
-  )
+  if (open) {
+    const data = JSON
+      .parse(localStorage.getItem(KEY))
+      .map((item) => ({ ...item, read: item.id === id ? true : item.read }))
 
-  target.getElementsByTagName('summary')[0].classList.remove('unread')
+    localStorage.setItem(KEY, JSON.stringify(data))
+    currentTarget.querySelector('summary').classList.remove('unread')
+  }
 }
 
 const render = (items) => {
-  document.getElementsByTagName('section')[0].innerHTML = items.map((item) => `
-    <details name="feed" id="${item.id}" ontoggle="onClick(event)">
+  document.querySelector('section').innerHTML = items.map((item) => `
+    <details name="feed" id="${item.id}" ontoggle="onToggle(event)">
       <summary class="${item.read ? '' : 'unread'}">${item.title}</summary>
       <p>
         <i>${item.date.split('T')[0].split('-').reverse().join('/')}</i>
@@ -34,32 +30,24 @@ const render = (items) => {
 fetch('https://nameless-bread-889b.jbonigomes.workers.dev/')
   .then((response) => response.text())
   .then((text) => {
-    const parser = new DOMParser()
-    const xmlDoc = parser.parseFromString(text, 'application/xml')
+    const xml = new DOMParser().parseFromString(text, 'application/xml')
+    const localItems = JSON.parse(localStorage.getItem(KEY) ?? '[]')
+    const seen = new Set(localItems.map(({ id }) => id))
 
-    const localItems = JSON.parse(localStorage.getItem(KEY) ?? '{}')
-    const fetchedItems = [...xmlDoc.getElementsByTagName('item')]
-      .filter((item) => !localItems[getText(item, 'guid')])
-      .reduce((acc, cur) => ({
-        ...acc,
-        [getText(cur, 'guid')]: {
-          read: false,
-          title: getText(cur, 'title'),
-          content: getText(cur, 'content:encoded'),
-          date: (new Date(getText(cur, 'pubDate'))).toJSON(),
-        },
-      }), {})
+    const fetchedItems = [...xml.getElementsByTagName('item')]
+      .map((item) => ({
+        read: false,
+        id: getText(item, 'guid'),
+        title: getText(item, 'title'),
+        content: getText(item, 'content:encoded'),
+        date: new Date(getText(item, 'pubDate')).toJSON(),
+      }))
+      .filter(({ id }) => !seen.has(id))
 
-    const allItems = Object
-      .entries({ ...fetchedItems, ...localItems })
-      .map(([id, item]) => ({ id, ...item }))
-      .toSorted((a, b) => b.date.localeCompare(a.date))
+    const allItems = [...fetchedItems, ...localItems]
+      .toSorted((a, b) => (a.read - b.read) || b.date.localeCompare(a.date))
       .slice(0, 50)
 
-    const read = allItems.filter(({ read }) => read)
-    const unread = allItems.filter(({ read }) => !read)
-
-    localStorage.setItem(KEY, JSON.stringify(allItems.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {})))
-
-    render([...unread, ...read])
+    localStorage.setItem(KEY, JSON.stringify(allItems))
+    render(allItems)
   })
